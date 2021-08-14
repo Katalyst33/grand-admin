3830cd6980f8b111847131070ab05995b22ecee4
Your Name <you@example.com>
2021-08-14 13:21:49 +0100

    install xpresser mongo

---
+++ backend/models/AppConfig.ts
@@ -0,0 +1,34 @@
+import {is, XMongoSchema, XMongoModel} from 'xpress-mongo';
+import {UseCollection} from '@xpresser/xpress-mongo';
+
+/**
+ * Interface for Model's `this.data`.
+ * Optional if accessing data using model helper functions
+ */
+export interface AppConfigDataType {
+    updatedAt?: Date,
+    createdAt: Date,
+}
+
+/**
+ * AppConfig Model
+ * Collection: `app_configs`
+ */
+class AppConfig extends XMongoModel {
+
+    // Set Model Schema
+    static schema: XMongoSchema = {
+        updatedAt: is.Date(),
+        createdAt: is.Date().required()
+    };
+
+    public data!: AppConfigDataType;
+}
+
+/**
+* Map Model to Collection: `app_configs`
+* .native() will be made available for use.
+*/
+UseCollection(AppConfig, "app_configs");
+
+export default AppConfig;
