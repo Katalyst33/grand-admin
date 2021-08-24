00575d9e4ef743f829f9c90019afc85facf2919b
Your Name <you@example.com>
2021-08-24 21:43:09 +0100

    deals end point middle ware adjustment

--- backend/jobs/SeedDeals.ts
+++ backend/jobs/SeedDeals.ts
@@ -3,6 +3,7 @@ import Deal from "../models/Deal";
 const chance = require("chance").Chance();
 
 import moment from "moment";
+import { randomInt } from "crypto";
 
 /**
  *  Job: SeedDeals
@@ -13,14 +14,71 @@ export = {
     // Your Job Here
     await Deal.native().deleteMany({});
 
-    const activity = [
-      "Travel",
-      "School",
-      "Romance",
-      "Vacation",
-      "Seminars",
-      "Religious",
+    const activities = [
+      "Travel to",
+      "School at",
+      "Romance in",
+      "Vacation in",
+      "Seminars in",
+      "Religious at",
     ];
+
+    const countryArray = [
+      {
+        full: "Nigeria",
+        code: "NG",
+      },
+      {
+        full: "United Kingdom",
+
+        code: "GB",
+      },
+      {
+        full: "Australia",
+
+        code: "AU",
+      },
+      {
+        full: "United states of America",
+
+        code: "US",
+      },
+      {
+        full: "Dubai",
+
+        code: "AE",
+      },
+      {
+        full: "Ireland",
+        code: "IE",
+      },
+      {
+        full: "Turkey",
+        code: "TR",
+      },
+      {
+        full: "Ukraine",
+        code: "UA",
+      },
+      {
+        full: "South Africa",
+        code: "ZA",
+      },
+      {
+        full: "Maldives",
+        code: "MV",
+      },
+    ];
+
+    let countryCodeString: string | never[] = [];
+    let countryFullString = null;
+
+    function countryCode() {
+      for (let value of countryArray) {
+        countryCodeString = value.code;
+        countryFullString = value.full;
+      }
+    }
 
     const country = [
       "Nigeria",
@@ -48,14 +106,18 @@ export = {
     let counter = 0;
 
     do {
+      const country = countryArray[randomInt(0, countryArray.length - 1)];
+      const activity = activities[randomInt(0, activities.length - 1)];
       const deals = {
-        title: `${activity[chance.integer({ min: 0, max: 5 })]} in/at/ ${
-          country[chance.integer({ min: 0, max: 12 })]
-        } , this ${chance.month()}`,
-        description: chance.sentence(),
-        country: chance.country({ full: true }),
-        expiresIn: "2021-08-15T20:43:34.489+00:00",
-        price: chance.integer({ min: 150000, max: 650000 }),
+        title: `${activity}  ${country.full} , this ${chance.month()}`,
+        description: `${activity}  ${
+          country.full
+        } , this ${chance.month()}, ${chance.sentence()}`,
+        countryCode: country.code,
+        country: country.full,
+        activity,
+        expiresIn: new Date("2021-08-15T20:43:34.489+00:00"),
+        price: randomInt(150000, 650000),
         included: [
           "Economy Class Ticket",
           "5 nights in Beach resort",
