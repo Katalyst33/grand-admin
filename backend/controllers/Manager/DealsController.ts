cc6cef5aeef85baf12731c8b333dd4d8fc6be5ab
katalyst 33 <you@example.com>
2021-12-12 13:14:47 +0000

    refactor deals

--- backend/controllers/Manager/DealsController.ts
+++ backend/controllers/Manager/DealsController.ts
@@ -30,7 +30,12 @@ export = <
     const data: Record<string, any> = {};
     if (http.hasParam("dealId")) {
       const dealId = http.params.dealId;
-      data.deal = await Deal.findOne({ uuid: dealId });
+      data.deal = await Deal.findOne(
+        { uuid: dealId },
+        {
+          projection: Deal.projectPublicFields(),
+        }
+      );
       //
       if (!data.deal) {
         return http.status(404).send({ error: "Destination not found" });
